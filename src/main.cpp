a6991caefa9e89718ad240d1b8f385902d38bc6c
Carl Westman <carl.westman@toyota-industries.eu>
2022-02-07 09:10:47 +0100

    Added Poured line

--- src/main.cpp
+++ src/main.cpp
@@ -6,25 +6,27 @@ HX711 scale;
 
 #define calibration_factor -25300.0
 #define zero_factor -31452.0
+#define keg_weight 4117.0 // grams
 
 #define LOADCELL_DOUT_PIN  3
 #define LOADCELL_SCK_PIN  2
 
 float weight;
+float poured_volume;
 
 void setup() {
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
   scale.set_scale(calibration_factor);
-  scale.tare();
+  scale.set_offset(zero_factor);
 
   lcd.init();
   lcd.backlight();
-  lcd.setCursor(0,0);
-  lcd.print("Weight [g]:");
 }
 
 void loop() {
-  weight = scale.get_units();
+  poured_volume = 12.0;
+
+  weight = scale.get_units(10);
   weight = weight*1000;
   if (weight < 5) {
     weight = 0;
@@ -32,8 +34,14 @@ void loop() {
 
   lcd.clear();
   lcd.setCursor(0,0);
-  lcd.print("Weight [g]:");
-  lcd.setCursor(0, 1);
-  lcd.print(weight,0);
-  delay(1000);
+  lcd.print("Volume:");
+  lcd.setCursor(8, 0);
+  lcd.print((weight-keg_weight)/1000,2);
+  lcd.print(" L");
+  lcd.setCursor(0,1);
+  lcd.print("Poured:");
+  lcd.setCursor(8, 1);
+  lcd.print(poured_volume,2);
+  lcd.print(" L");
+  delay(100);
 }
